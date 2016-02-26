f0839b8a21009c42878c63525e17127f8159db7f
doroncy <doron.cy@gmail.com>
2016-02-26 09:15:39 +0200

    gallery

--- src/app.js
+++ src/app.js
@@ -68,7 +68,12 @@ const App = React.createClass({
           <div className="footer-wrap">
             <div className="row">
               <div className="small-12 medium-10 columns small-centered">
-                <div className="row">
+                <div className="row no-margin-left no-margin-right">
+                  <div className="small-12 columns medium-2 show-for-small-only">
+                    <a href="https://soundcloud.com/aria-bar" target="_blank">
+                      <div className="SVGIcon icon-icon_SOUNDCLOUDE icon-icon_SOUNDCLOUDE-dims centered"></div>
+                    </a>
+                  </div>
                   <div className="small-12 columns medium-5 aria-copy no-padding-left">
                     <div className="footer-text">Copyright 2016 ARIA. All Rights Reserved.</div>
                   </div>
--- src/components/upstairs/upstairs.js
+++ src/components/upstairs/upstairs.js
@@ -9,8 +9,16 @@ import menuItems from './menu';
 const upstairsInfo = {
   title: 'The restaurant',
   description: 'blah lbah blalajsa sdjflksd jflksdjflksd jflksd jflksdj flk',
-  bg: 'upstairs-bg'
+  bg: 'upstairs-bg',
+  items: []
 }
+
+let galleryInfo = {
+  title: 'Gallery',
+  bg: 'upstairs-gallery-1',
+  items: []
+}
+
 
 export default class Upstairs extends Component {
   constructor(props) {
@@ -18,14 +26,17 @@ export default class Upstairs extends Component {
 
     this.state = {
       currentTab: upstairsInfo,
-      tabIndex: 0
+      tabIndex: 0,
+      galleryIndex: 1
     };
+
+    this.nextImage = this.nextImage.bind(this);
   }
 
   componentDidMount() {
     var mainElem = this.refs.contentBody;
     var menuItemsKeys = _.keys(menuItems);
-    
+
     let that = this;
     mainElem.onscroll = function(ev) {
       if ((mainElem.scrollTop + mainElem.clientHeight) >= mainElem.scrollHeight) {
@@ -46,11 +57,28 @@ export default class Upstairs extends Component {
     });
   }
 
+  prevImage() {
+    let newGalleryIndex = this.state.galleryIndex === 1 ? 7 : this.state.galleryIndex - 1;
+    galleryInfo.bg = `upstairs-gallery-${newGalleryIndex}`;
+    this.setState({
+      galleryIndex: newGalleryIndex
+    });
+  }
+
+  nextImage() {
+    let newGalleryIndex = this.state.galleryIndex === 7 ? 1 : this.state.galleryIndex + 1;
+    galleryInfo.bg = `upstairs-gallery-${newGalleryIndex}`;
+    this.setState({
+      galleryIndex: newGalleryIndex
+    });
+  }
+
   render() {
     let tabs = _.map(menuItems, (menuItem, index) => {
+      let isTabSelected = this.state.tabIndex === index ? "selected" : "";
       return (
         <div key={index} className="medium-2 columns xs-padding-sides">
-          <div className="content-box content-box-btn" onClick={this.changeTab.bind(this,menuItem, index)}>
+          <div className={`content-box content-box-btn ${isTabSelected}`} onClick={this.changeTab.bind(this,menuItem, index)}>
             {menuItem.title}
           </div>
         </div>
@@ -58,6 +86,10 @@ export default class Upstairs extends Component {
     });
 
     let contentBody;
+    let galleryModeClass = '';
+    let gallerySelected = '';
+    let galleryArrows = '';
+    console.log(this.state.currentTab);
     if (this.state.currentTab === upstairsInfo) {
       contentBody = (
         <div>
@@ -66,27 +98,42 @@ export default class Upstairs extends Component {
           <div className="upstairs-info-desc font-light">{upstairsInfo.description}</div>
         </div>
       );
+    } else if (this.state.currentTab === galleryInfo) {
+      galleryModeClass = 'gallery'
+      gallerySelected = 'selected'
+      galleryArrows = (
+        <div className="gallery-icons">
+          <div className="SVGIcon icon-arrow_left" onClick={this.prevImage.bind(this)}></div>
+          <div className="SVGIcon icon-arrow_right" onClick={this.nextImage.bind(this)}></div>
+        </div>
+      )
     } else {
       contentBody = this.state.currentTab.items.map((item, index) => {
         return (
           <li className="menuitem" key={item.name}>
-            <div className="menuitem-name font-SemiBold ">{item.name}</div>
+            <div className="menuitem-name font-SemiBold">{item.name}</div>
             <p className="menuitem-description font-light">{item.description}</p>
           </li>
         );
       });
-      contentBody = (<ul className="no-bullet" ref="contentList">{contentBody}</ul>);
+      contentBody = (
+        <div>
+          <div className="menu-title font-ExBold">{this.state.currentTab.title}</div>
+          <ul className="no-bullet" ref="contentList">{contentBody}</ul>
+        </div>
+      );
     }
 
     return(
-      <div className={`animated fadeIn height100 background background-fade ${this.state.currentTab.bg}`}>
-        <div className="tabs-component">
+      <div className={`pos-relative animated fadeIn height100 background background-fade ${this.state.currentTab.bg}`}>
+        {galleryArrows}
+        <div className={`tabs-component ${galleryModeClass}`}>
           <div className="row">
             <div className="mall-12 medium-10 columns small-centered">
               <div className="row">
-                <div className="small-12 columns small-centered xs-padding-sides">
+                <div className="small-12 columns small-centered">
                   <div className="content-box tabs-header" onClick={this.changeTab.bind(this,upstairsInfo)}>
-                    <div className="SVGIcon icon-Upstairs_for_menu"></div>
+                    <div className="SVGIcon upstairs-menu-title"></div>
                   </div>
                 </div>
               </div>
@@ -94,49 +141,40 @@ export default class Upstairs extends Component {
                 {tabs}
               </div>
               <div className="row show-for-small-only">
-                <div className="small-12 columns xs-padding-sides">
+                <div className="small-12 columns">
                   <div className="content-box content-box-btn" onClick={this.changeTab.bind(this,menuItems['entrees'], 'entrees')}>
                     Menu
                   </div>
                 </div>
               </div>
-              <div className="row">
-                <div className="small-12 columns small-centered xs-padding-sides pos-relative">
-                  <div className="content-box content-box-no-hover main-container xl-padding" ref="contentBody">
+              <div className="row content-body-wrap">
+                <div className="small-12 columns small-centered pos-relative">
+                  <div className="content-box content-box-no-hover main-container" ref="contentBody">
                     {contentBody}
                   </div>
                   <div className="fade-bottom"></div>
                 </div>
               </div>
-              <div className="action-btn-wrap">
-                <div className="action-btn-inner">
-                  <div className="row">
-                    <div className="small-6 columns xs-padding-sides">
-                      <div className="content-box table-btn">
-                        <div className="content-box-title content-box-title-small">Gallery</div>
-                      </div>
-                    </div>
-                    <div className="small-6 columns xs-padding-sides">
-                      <div className="content-box table-btn">
-                        <a href="http://clickiframe.clickatable.co.il/%D7%9E%D7%A1%D7%A2%D7%93%D7%AA-%D7%90%D7%A8%D7%99%D7%94-%D7%9E%D7%A1%D7%A2%D7%93%D7%94.aspx" target="_blank">
-                          <div className="content-box-title content-box-title-small">Reserve a table</div>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+              <div className="action-btn-wrap-flex">
+                <div className={`content-box content-box-btn table-btn ${gallerySelected}`} onClick={this.changeTab.bind(this,galleryInfo)}>
+                  <div className="content-box-title content-box-title-small">Gallery</div>
                 </div>
-                <div className="content-box social-media-btn facebook-btn">
+                <div className="content-box content-box-btn table-btn">
+                  <a href="http://clickiframe.clickatable.co.il/%D7%9E%D7%A1%D7%A2%D7%93%D7%AA-%D7%90%D7%A8%D7%99%D7%94-%D7%91%D7%A8.aspx" target="_blank">
+                    <div className="content-box-title content-box-title-small">Reserve a table</div>
+                  </a>
+                </div>
+                <div className="content-box content-box-btn social-media-btn facebook-btn">
                   <a href="https://www.facebook.com/ariatlv" target="_blank">
                     <div className="SVGIcon icon-icon_FACEBOOK"></div>
                   </a>
                 </div>
-                <div className="content-box social-media-btn instagram-btn">
+                <div className="content-box content-box-btn social-media-btn instagram-btn">
                   <a href="https://www.instagram.com/ariatlv/" target="_blank">
                     <div className="SVGIcon icon-icon_INSTAGRAM"></div>
                   </a>
                 </div>
               </div>
-
 
             </div>
           </div>
