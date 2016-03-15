60e9ef4eb686b470e5633e1464b091bbd5bd13dc
doroncy <doron.cy@gmail.com>
2016-03-16 00:42:08 +0200

    final touches

--- src/components/music/music.js
+++ src/components/music/music.js
@@ -30,8 +30,7 @@ export default class Team extends Component {
     super();
 
     this.state = {
-      showGallery: false,
-      images: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+      showGallery: false
     };
 
     this.pageData = {
